8bc8beeef8ffe6cf66d0e2b424098b43cc534ae7
DewashishTikas <tikasdewashish2208@gmail.com>
2025-07-26 19:06:38 +0530

    fix error

--- client/src/pages/AdminPost.jsx
+++ client/src/pages/AdminPost.jsx
@@ -1,38 +1,45 @@
 import React, { useEffect, useState } from "react";
 import InputComp from "../Components/InputComp";
 import deleteIcon from "../assets/images/deleteicon.svg";
-import Button from "../Components/Button";
 import { useNavigate } from "react-router";
 
 const AdminPost = () => {
   const navigate = useNavigate();
-  const [post, setPost] = useState();
+  const [post, setPost] = useState("");
   const [message, setMessage] = useState("");
   const [vancancies, setVacancies] = useState([]);
-  useEffect( () => {
-    (async () => {
+  async function fetchData() {
     try {
-      const response = await fetch(`${import.meta.env.VITE_SERVER_BASE_URL}/user/vacancy`);
+      const response = await fetch(
+        `${import.meta.env.VITE_SERVER_BASE_URL}/user/vacancy`
+      );
       const data = await response.json();
-     if(data.error )return;
-      setVacancies(data);
+      if (data.error) return;
+      setVacancies(data.data);
+      console.log(vancancies);
     } catch (err) {
       console.log(err);
     }
+  }
+  useEffect(() => {
+    (async() => {
+      await fetchData();
     })();
   }, []);
-
   async function handleSubmit(e) {
-   e.preventDefault();
+    e.preventDefault();
     try {
-      const response = await fetch(`${import.meta.env.VITE_SERVER_BASE_URL}/admin/vacancy`, {
-        method: "POST",
-        credentials: "include",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ post }),
-      });
+      const response = await fetch(
+        `${import.meta.env.VITE_SERVER_BASE_URL}/admin/vacancy`,
+        {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ post }),
+        }
+      );
       if (response.status === 403) {
         console.log("Unauthorized access");
         navigate("/adminLogin");
@@ -43,6 +50,7 @@ const AdminPost = () => {
       const data = await response.json();
       if (response.status === 200) {
         setMessage(data.message || "Post Added");
+        await fetchData();
       } else {
         setMessage(data.error || "Something went wrong");
       }
@@ -53,14 +61,17 @@ const AdminPost = () => {
 
   async function handleDelete(vacancyId) {
     try {
-      const response = await fetch(`${import.meta.env.VITE_SERVER_BASE_URL}/admin/vacancy`, {
-        method: "DELETE",
-        credentials: "include",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ vacancyId }),
-      });
+      const response = await fetch(
+        `${import.meta.env.VITE_SERVER_BASE_URL}/admin/vacancy`,
+        {
+          method: "DELETE",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ vacancyId }),
+        }
+      );
       if (response.status === 403) {
         console.log("Unauthorized access");
         navigate("/adminLogin");
@@ -68,6 +79,7 @@ const AdminPost = () => {
       const data = await response.json();
       if (response.status === 200)
         setMessage(data.message || "Vacancy Deleted Successfully");
+      await fetchData();
     } catch (err) {
       console.log(err);
     }
@@ -88,27 +100,38 @@ const AdminPost = () => {
           <InputComp value={post} setValue={setPost} type={"text"}>
             Enter Post
           </InputComp>
-          <button className="bg-blue-500 text-white px-4 py-2 my-5 rounded-md">Add</button>
+          <button className="bg-blue-500 text-white px-4 py-2 my-5 rounded-md">
+            Add
+          </button>
         </form>
-        {message && <p>{message}</p>}
-        <div className="mt-10">
+        {message && <p className="relative">{message}</p>}
+        <div className=" relative top-10">
           <h1 className="text-2xl my-3">Vacancies</h1>
-          {!!vancancies.length && vancancies.map((vacancy) => {
-            return (
-              <>
-                <div className="flex justify-between text-lg" key={vacancy.id}>
-                  <p>{vacancy.name}</p>
-                  <button
-                    onClick={(e) => {
-                      handleDelete(vacancy.id);
-                    }}
+          {!!vancancies.length &&
+            vancancies.map((vacancy) => {
+              return (
+                <>
+                  <div
+                    className="flex justify-between text-lg"
+                    key={vacancy.id}
                   >
-                    <img src={deleteIcon} className="h-10 w-10" alt="" />
-                  </button>
-                </div>
-              </>
-            );
-          })}
+                    <p>{vacancy.name}</p>
+                    <button
+                      onClick={(e) => {
+                        handleDelete(vacancy.id);
+                        className = "cursor-pointer";
+                      }}
+                    >
+                      <img
+                        src={deleteIcon}
+                        className="h-10 w-10 cursor-pointer"
+                        alt=""
+                      />
+                    </button>
+                  </div>
+                </>
+              );
+            })}
         </div>
       </section>
     </>
