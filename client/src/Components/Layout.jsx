40dab6de5f83f175bb69e49462ef841954f02de3
DewashishTikas <tikasdewashish2208@gmail.com>
2025-07-31 14:01:35 +0530

    add loader

--- client/src/Components/Layout.jsx
+++ client/src/Components/Layout.jsx
@@ -1,6 +1,7 @@
 import { lazy, Suspense, useEffect, useState } from "react";
 import { Outlet, useLocation } from 'react-router';
 import Header from "./Header";
+import LoadingPage from "./LoadingPage";
 const Footer = lazy(()=> import('./Footer'))
 
 
@@ -17,7 +18,23 @@ const Layout = () => {
     <>
       <Header Ypos={Ypos} />
       <main className="overflow-hidden">
-        <Suspense>{<Outlet context={{ setCurrentRef }} />}</Suspense>
+        <Suspense
+          fallback={
+            <div className="min-h-screen flex items-center justify-center">
+              <LoadingPage />
+            </div>
+          }
+        >
+          <Suspense
+            fallback={
+              <div className="min-h-screen flex items-center justify-center">
+                <LoadingPage />
+              </div>
+            }
+          >
+            {<Outlet context={{ setCurrentRef }} />}
+          </Suspense>
+        </Suspense>
       </main>
       <Suspense>
         <Footer />
